7ae1d63ab271681b9e6107f20d1b437cd3cdb080
vadim epstein <eps@here.ru>
2022-03-31 21:10:15 +0300

    version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aphantasia',
-    version='2.2.0',
+    version='2.3.0',
     description='CLIP + FFT/DWT/RGB text-to-image tools',
     url='https://github.com/eps696/aphantasia',
     author='vadim epstein',
